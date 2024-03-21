6c03eef477b173a84164f776d592c3c2c1a8f691
Eg0r1cKeN <egorkyznetsov06@gmail.com>
2024-03-21 13:08:42 +0300

    Убрано лишнее

--- data/users.py
+++ data/users.py
@@ -17,7 +17,6 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    news = orm.relationship("News", back_populates='user')
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
