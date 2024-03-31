6989a20def5baf1874b01bb2780a98409fa94bb6
Eg0r1cKeN <egorkyznetsov06@gmail.com>
2024-03-31 22:10:22 +0300

    Начат процесс конвертации

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask_login import LoginManager, logout_user, login_required, current_user, login_user
-from flask import Flask, render_template, url_for, redirect, request, make_response, session, abort, jsonify
-
-
+from flask import Flask, render_template, redirect, make_response, jsonify, request, send_from_directory, send_file
+from flask_login import LoginManager, logout_user, login_required, login_user, current_user, AnonymousUserMixin
+from flask_restful import Api
 from data.users import User
+from data.user_file import UserFile
+from data import db_session
 from forms.loginform import LoginForm
 from forms.user import RegisterForm
-from data import db_session
-from flask_restful import abort, Api
+from io import BytesIO
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -22,10 +23,22 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        print(1)
+        files = db_sess.query(UserFile).filter(UserFile.owner == current_user.id).all()
+    else:
+        files = None
+    return render_template("index.html", files=files)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -52,7 +65,6 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -73,7 +85,6 @@ def login():
 def logout():
     logout_user()
     return redirect("/")
-
 
 
 @app.route('/update_user')
@@ -98,6 +109,54 @@ def new_user():
         db_sess.commit()
     print(user.name)
     return user.name
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+@login_required
+def upload():
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        user = current_user
+        file = request.files['file']
+        db_file = UserFile()
+        db_file.filename = file.filename
+        db_file.owner = user.id
+        db_file.data = file.read()
+        db_sess.add(db_file)
+        db_sess.commit()
+        return redirect("/")
+    return render_template('file_upload.html')
+
+
+# конвертация файлов разных фогрматов
+
+@app.route('/conversion/<upload_id>')
+@login_required
+def conversion(upload_id):
+    return render_template('conversion.html', name=conversion)
+
+
+# create download function for download files
+@app.route('/download/<upload_id>')
+@login_required
+def download(upload_id):
+    db_sess = db_session.create_session()
+    file = db_sess.query(UserFile).filter_by(id=upload_id).first()
+    if current_user.id == file.owner == current_user.id:
+        return send_file(BytesIO(file.data), download_name=file.filename, as_attachment=True)
+    else:
+        return
+
+
+@app.route('/delete/<upload_id>')
+@login_required
+def delete(upload_id):
+    db_sess = db_session.create_session()
+    file = db_sess.query(UserFile).filter_by(id=upload_id).first()
+    if current_user.id == file.owner == current_user.id:
+        db_sess.delete(file)
+        db_sess.commit()
+        return redirect("/")
 
 
 @app.errorhandler(404)
