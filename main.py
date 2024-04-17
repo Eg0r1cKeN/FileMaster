271e73cd283330d1700f9d8553a4e77dd22d9d9e
TTTap1xxx <andreytap1x@gmail.com>
2024-04-18 00:58:58 +0300

    Дизайн сайта приближен к общему виду, добавлны аватары пользователя, система пользователей переделана на основе логинов, добавленно удаление аккаунтов и логотип сайта в также страцица сайта, редактирования профиля.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import shutil
 from flask import Flask, render_template, redirect, make_response, jsonify, request, send_from_directory, send_file
 from flask_login import LoginManager, logout_user, login_required, login_user, current_user, AnonymousUserMixin
 from flask_restful import Api
@@ -7,9 +6,9 @@ from data.user_file import UserFile
 from data import db_session
 from forms.loginform import LoginForm
 from forms.user import RegisterForm
-from io import BytesIO
 import os
-from file_upload import Upload
+from waitress import serve
+import shutil
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -27,22 +26,22 @@ def load_user(user_id):
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/files')
+@login_required
 def index():
     db_sess = db_session.create_session()
     if current_user.is_authenticated:
         files = db_sess.query(UserFile).filter(UserFile.owner == current_user.id).all()
-        print(current_user.id)
         if not os.path.isdir(os.path.join(os.getcwd(), f'files/id_user_{current_user.id}')):
             os.mkdir(os.path.join(os.getcwd(), f'files/id_user_{current_user.id}'))
     else:
         files = None
-    print()
     return render_template("index.html", files=files)
+
 
 @app.route('/')
 @app.route('/index')
@@ -58,21 +57,18 @@ def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
+            return render_template('register.html', title='Регистрация', form=form,
                                    message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.name == form.name.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
+            return render_template('register.html', title='Регистрация', form=form,
                                    message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-        )
+        user = User(name=form.name.data)
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
+        shutil.copyfile('static/img/avatar/default_avatar.png', f'files/avatar/{current_user.id}.png')
+        return redirect('/files')
     return render_template('register.html', title='Регистрация', form=form)
 
 
@@ -85,9 +81,7 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
@@ -96,6 +90,11 @@ def login():
 def logout():
     logout_user()
     return redirect("/")
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -123,7 +122,8 @@ def download(upload_id):
     file = db_sess.query(UserFile).filter_by(id=upload_id).first()
     user = current_user
     if current_user.id == file.owner == current_user.id:
-        return send_file(f"files/id_user_{user.id}/{file.filename}", download_name=file.filename, as_attachment=True)
+        return send_file(f"files/id_user_{user.id}/{file.filename}", download_name=file.filename,
+                         as_attachment=True)
     else:
         return
 
@@ -144,17 +144,38 @@ def delete(upload_id):
         return redirect("/")
 
 
+@app.route('/delete_account')
+@login_required
+def delete_account():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).get(current_user.id)
+    db_sess.delete(user)
+    db_sess.commit()
+    shutil.rmtree(f"files/id_user_{user.id}")
+    return redirect("/")
+
+
+@app.route('/upload_avatar', methods=['GET', 'POST'])
+@login_required
+def upload_avatar():
+    if request.method == 'POST':
+        file = request.files['file']
+        os.remove(f'static/img/avatar/{current_user.id}.png')
+        file.save(f'static/img/avatar/{current_user.id}.png')
+    return redirect('/')
+
+
+
 @app.errorhandler(404)
-def not_found(error):
-    print(error)
+def not_found():
     return make_response(jsonify({'error': '404 Not Found'}), 404)
 
 
 @app.errorhandler(400)
-def bad_request(_):
+def bad_request():
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 if __name__ == '__main__':
     db_session.global_init("db/database.db")
-    app.run(port=8080, host='127.0.0.1')
+    serve(app, port=80, host='127.0.0.1')
